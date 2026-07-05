0d853b5bb56d46f14515ab4cd788a64142810ef9
Luzivam Santos <luzivam.santos28@outlook.com>
2026-07-05 20:43:34 -0300

    adiciona troca de modulos com JavaScript

---
+++ src/js/script.js
@@ -0,0 +1,15 @@
+function trocarModulo(idDoModulo) {
+    const modulos = document.querySelectorAll(".modulo");
+
+    modulos.forEach(function (modulo) {
+        modulo.hidden = modulo.id !== idDoModulo;
+    });
+}
+
+const botoesModulo = document.querySelectorAll("[data-modulo]");
+
+botoesModulo.forEach(function (botao) {
+    botao.addEventListener("click", function () {
+        trocarModulo(botao.dataset.modulo);
+    });
+});
